7ef00379d9030063a470b609a9c1e0b56f2798e1
Gigasitron X-Cross <133568501+Gigasitron-xcross@users.noreply.github.com>
2026-07-27 11:49:19 +0800

    Add first XC_GUI block

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-basic.forever(function () {
-	
-})
+/**
+ * XC_GUI graphics support for Gigasitron LCD modules.
+ */
+//% color="#1976D2" weight=90 icon="\uf108" block="XC_GUI"
+namespace xcGUI {
+    /**
+     * Test that the XC_GUI extension is working.
+     */
+    //% blockId=xc_gui_test
+    //% block="test XC_GUI extension"
+    //% weight=100
+    export function testExtension(): void {
+        basic.showString("XC")
+    }
+}
